74135c77e1763bccebce7d0e0c1c8492d0a0b68b
i9Workshop <i9.workroom@gmail.com>
2024-09-19 19:04:06 +0800

    Update program.h

--- Tutorials-PIC16F/1-LED-Blink/StarterBoardV1-PIC16F1783-ClockConfigAndLedBlink.X/program.h
+++ Tutorials-PIC16F/1-LED-Blink/StarterBoardV1-PIC16F1783-ClockConfigAndLedBlink.X/program.h
@@ -8,8 +8,8 @@ extern "C" {
 #endif
     
     
-#define led1        LATBbits.LATB6
-#define led2        LATBbits.LATB7
+#define led1        LATBbits.LATB7
+#define led2        LATBbits.LATB6
 
 
     void delay_x1o5us(uint8_t delay);
